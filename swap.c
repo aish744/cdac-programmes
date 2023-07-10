855dd2fbdd55ed2dbac54b8b596dd7521149b0c1
Aishwarya Swami <90538432+aish744@users.noreply.github.com>
2023-07-10 16:37:02 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 
-int main(){
-    int a=1,b=2,temp;
-    temp = a;
+void swap(int a,int b,int temp)
+{
+    temp=a;
     a=b;
     b=temp;
     printf("swapped numbers are a=%d,b=%d", a,b);
+}
+
+int main(){
+    int a=1,b=2,temp;
+    // temp = a;
+    // a=b;
+    // b=temp;
+    // printf("swapped numbers are a=%d,b=%d", a,b);
+    swap(a,b,temp);
     return 0;
 }
