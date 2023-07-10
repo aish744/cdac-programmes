38c77aa828c5bb58ab0295e8982653399078fb04
Aishwarya Swami <90538432+aish744@users.noreply.github.com>
2023-07-10 16:37:38 +0530

    Update sumofdigits.c

--- sumofdigits.c
+++ sumofdigits.c
@@ -1,17 +1,34 @@
 #include<stdio.h>
 
-int main(){
-    int n,r,sum=0;
+int Sum(int n)
+{
+    int r,sum=0;
     printf("enter a number: ");
     scanf("%d",&n);
 
     while(n>0)
     {
         r=n%10;
-        sum = sum +r;
+        sum=sum+r;
         n=n/10;
     }
     printf("the sum is= %d",sum);
+
+}
+
+int main(){
+    int n,r,sum;
+    // printf("enter a number: ");
+    // scanf("%d",&n);
+
+    // while(n>0)
+    // {
+    //     r=n%10;
+    //     sum = sum +r;
+    //     n=n/10;
+    // }
+    Sum(n);
+    // printf("the sum is= %d",sum);
     return 0;
  
 
