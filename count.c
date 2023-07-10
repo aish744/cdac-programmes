4ee76a5667874b9ad859dff9459f036fbc95c2a4
Aishwarya Swami <90538432+aish744@users.noreply.github.com>
2023-07-10 16:34:54 +0530

    Add files via upload

--- count.c
+++ count.c
@@ -1,46 +1,67 @@
 #include<stdio.h>
-
-void count(FILE* file)
+void count(FILE* file,int* vowel, int* consonant, int* space)
 {
-    int vowel=0;
-    int consonant=0;
-    int space=0;
+    * vowel=0;
+    * consonant=0;
+    * space=0;
 
-     char c;
+    char c;
 
     for (c=fgetc(file);c!=EOF;c=fgetc(file))
     {
         if(c== 'a'|| c=='e'|| c=='i'|| c=='o'|| c=='u' ||
             c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
         {
-            (vowel)++;
+            (*vowel)++;
         }
         else if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
         {
-            (consonant)++;
+            (*consonant)++;
         }
          else if (c == ' ') {
-            (space)++;
+            (*space)++;
         }
         
     }
-    printf("vowels:%d\n",vowel);
-    printf("consonant:%d\n",consonant);
-    printf("space:%d\n",space);
-    
 }
-
 int main()
 {
-    
-    FILE* file=fopen("source","r");
-    // int vowel=0;
-    // int consonant=0;
-    // int space=0;
-    count(file);
+    FILE* file= fopen("source","r");
+
+    int space=0;
+    int vowel=0;
+    int consonant=0;
+    if (file == NULL) {
+        printf("Error: Failed to open the file\n");
+        return 1;
+    }
+    // char c;
+
+    // for (c=fgetc(file);c!=EOF;c=fgetc(file))
+    // {
+    //     if(c== 'a'|| c=='e'|| c=='i'|| c=='o'|| c=='u' ||
+    //         c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+    //     {
+    //         vowel++;
+    //     }
+    //     else if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    //     {
+    //         consonant++;
+    //     }
+    //      else if (c == ' ') {
+    //         space++;
+    //     }
+    //     // if(c == " \n")
+    //     // {
+    //     //     space++;
+    //     // }
+    // }
+    count(file,&vowel,&consonant,&space);
     fclose(file);
-    // printf("vowels:%d",vowel);
-    // printf("consonant:%d",consonant);
-    // printf("space:%d",space);
+    printf("number of voweles are: %d \n",vowel);
+    printf("number of consatant are: %d \n",consonant);
+    printf("number of spaces are: %d \n",space);
+
     return 0;
+
 }
